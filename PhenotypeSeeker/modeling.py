8aecf810961ef251e2af0da505481ac05a35be54
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-07-31 11:17:00 +0300

    skip kmer_matrix

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -302,7 +302,11 @@ def weighted_t_test(
     f2 = open(outputfile, "w+")
     opened_kmer_lists = [open(item) for item in split_of_kmer_lists]
     for pre_line in izip_longest(*opened_kmer_lists, fillvalue = ''):
-        f2.write(pre_line[1].split()[0] + '\t' + '\t'.join(j.strip(pre_line[1].split()[0]).strip() for j in pre_line) + "\n")
+        counter += 1
+        try:
+            f2.write(pre_line[1].split()[0] + '\t' + '\t'.join(j.strip(pre_line[1].split()[0]).strip() for j in pre_line) + "\n")
+        except:
+            print("Error in file: " + str(split_of_kmer_lists[0][-5:]) + " line: " + counter)
         '''
         counter += 1
         samp_w_pheno_specified = 0
