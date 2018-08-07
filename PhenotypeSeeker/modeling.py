2b7ef2b0f4cfd1e63ba3b1857fb554f296f54e09
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-08-07 12:21:14 +0300

    warnings suppressed

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -10,7 +10,7 @@ from subprocess import call, Popen, PIPE, check_output
 import math
 import sys
 import warnings
-warnings.showwarnings = lambda *args, **kwargs: None
+warnings.showwarning = lambda *args, **kwargs: None
 
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, _DistanceMatrix
 from cogent import LoadTree
