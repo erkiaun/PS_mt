37d48b21c105f50fd7d8c4859ebdef3252adbbdc
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-08-14 16:14:01 +0300

    OOP

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -386,11 +386,11 @@ def test_kmers_association_with_phenotype(
         sys.stderr.write("\nConducting the k-mer specific Welch t-tests:\n")
     else:
         sys.stderr.write("\nConducting the k-mer specific chi-square tests:\n")
-    (
-    vectors_as_multiple_input, progress_checkpoint, no_kmers_to_analyse
-    ) = get_params_for_kmers_testing(
-        samples, num_threads, phenotypes_to_analyse
-        )
+    # (
+    # vectors_as_multiple_input, progress_checkpoint, no_kmers_to_analyse
+    # ) = get_params_for_kmers_testing(
+    #     samples, num_threads, phenotypes_to_analyse
+    #     )
 
     # for j, k in enumerate(phenotypes_to_analyse):
     #     currentKmerNum.value = 0
@@ -428,12 +428,10 @@ def get_params_for_kmers_testing(samples, num_threads, phenotypes_to_analyse):
 def _split_sample_vectors_for_multithreading(samples, num_threads):
     print(samples)
     for sample in samples:
-        call(
-            [
+        call([
             "split -a 5 -d -n r/" + str(num_threads) + " K-mer_lists/" +
             sample + "_mapped.txt " + "K-mer_lists/" + sample + "_mapped_"
-            ]
-            , shell=True)
+            ], shell=True)
 
 def _splitted_vectors_to_multiple_input(samples, num_threads):
     vectors_as_multiple_input = []
