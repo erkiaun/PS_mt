2fb0d816bea479f913734e5daf6ea951260a7511
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-08-16 03:07:37 +0300

    weights 1

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -551,9 +551,9 @@ def weighted_t_test(x, y, x_weights, y_weights):
     #Calculating the weighted Welch's t-test results
     dif = wtd_mean_x-wtd_mean_y
     sxy = math.sqrt((varx/sumofweightsx)+(vary/sumofweightsy))
+    print(sxy)
     df = (((varx/sumofweightsx)+(vary/sumofweightsy))**2)/((((varx/sumofweightsx)**2)/(sumofweightsx-1))+((vary/sumofweightsy)**2/(sumofweightsy-1)))
     t= dif/sxy
-    print(t)
     pvalue = stats.t.sf(abs(t), df)*2
 
     return t, pvalue, wtd_mean_x, wtd_mean_y
