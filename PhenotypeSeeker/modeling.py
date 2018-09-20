166d28b17182e183576d14903deef8629a5cdb92
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-09-20 21:15:03 +0300

    ud

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -415,7 +415,6 @@ class phenotypes():
 
     # -------------------------------------------------------------------
     # Functions for calculating the association test results for kmers.
-    @classmethod
     def test_kmers_association_with_phenotype(self):
         stderr_print.currentKmerNum.value = 0
         stderr_print.previousPercent.value = 0
