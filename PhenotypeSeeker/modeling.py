0de094d00883ca2df831b84b0b81f36830b68f71
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-10-08 12:37:25 +0300

    ud

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -1192,9 +1192,13 @@ class phenotypes():
     def cross_validation_results(self):
         if self.model_name_short not in ("RF", "NB", "XGBC", "XGBR"):
             self.summary_file.write('Parameters:\n%s\n\n' % self.model)
-            self.summary_file.write("Grid scores (R2 score) on development set: \n")
-            means = self.best_classifier.cv_results_['mean_test_score']
-            stds = self.best_classifier.cv_results_['std_test_score']
+            self.summary_file.write("Grid scores (R2 score) on development set: \n")s
+            if self.scale == "continuous":
+                means = self.best_regressor.cv_results_['mean_test_score']
+                stds = self.best_regressor.cv_results_['std_test_score']
+            elif self.scale == "binary":
+                means = self.best_classifier.cv_results_['mean_test_score']
+                stds = self.best_classifier.cv_results_['std_test_score']
             for mean, std, params in izip(
                     means, stds, self.best_classifier.cv_results_['params']
                     ):
