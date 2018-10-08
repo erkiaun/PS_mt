623f8955514cdbc88b96cc205e230e65f04a305f
Erki Aun <erkiaun@Erkis-MacBook-Pro.local>
2018-10-08 21:54:01 +0300

    update

--- PhenotypeSeeker/modeling.py
+++ PhenotypeSeeker/modeling.py
@@ -274,8 +274,8 @@ class Samples():
     # input samples.
     
     def get_mash_sketches(self):
-    	call(["cat", self.address, "|", "mash", "sketch", "-", "-o", "K-mer_lists/" + self.name], shell=True)
-    	# process = Popen(mash_args, shell=True, stderr=PIPE)
+    	mash_args = ["cat", self.address, "|", "mash", "sketch", "-", "-o", "K-mer_lists/" + self.name]
+    	process = Popen(mash_args, shell=True, stderr=PIPE)
 
     @classmethod
     def get_weights(cls):
